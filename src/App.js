5bb875e6468af5c592326e025415abf76acc8d31
Diana Rybikov <diana.ryb214@gmail.com>
2021-11-03 12:33:35 -0600

    fetch/fix

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 function App() {
 
   useEffect(() => {
-      fetch('http://localhost:300/flowers')
+      fetch('http://localhost:3000/flowers')
       .then(res => res.json())
       .then(flowers => console.log('flowers', flowers))
   });
