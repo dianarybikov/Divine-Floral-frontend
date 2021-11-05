81a35ff3bfb3c7e5dc3d6c3bcbb3ebb7e5ed12a8
Diana Rybikov <diana.ryb214@gmail.com>
2021-11-05 17:43:42 -0600

    container working

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import { useState ,useEffect } from 'react';
-import flowersContainer from './FlowersContainer'
-// import logo from './logo.svg';
+import FlowersContainer from './FlowersContainer'
+
 import './App.css';
 
 function App() {
 
   const [flowers, setFlowers] = useState([])
+  
+  const getFlowers = () => {
+  fetch('http://localhost:3000/flowers')
+  .then(res => res.json())
+  .then(apiFlowers => setFlowers(apiFlowers))
+ };
+  
+ useEffect(getFlowers, []);
 
-  useEffect(() => {
-      fetch('http://localhost:3000/flowers')
-      .then(res => res.json())
-      .then(apiFlowers => setFlowers(apiFlowers))
-  });
   return (
-    <div className="app">
-      <h1>Divine Floral</h1>
-      <p> this is my website for a side company that will be starting. currently work out the api</p>
-    </div>
+    <div className="App">
+    <FlowersContainer flowers={flowers}/>
+      </div>
   );
 }
 
 export default App;
+
+
--- src/FlowersContainer.js
+++ src/FlowersContainer.js
@@ -1,9 +1,18 @@
-
-
-function flowersContainer() {
+export default function flowersContainer({flowers}) {
+    const renderFlowers = () => {
+        return flowers.map(flower => {
+            return (
+                <div key={flower.id} className="flower-card">
+                    <h2>{flower.name}</h2>
+                    <p>cost: {flower.cost}</p>
+                    {/* <img>img: {flower.img}</img> */}
+                </div>
+            )
+        })
+    }
     return(
-        <section className="sumos-container">
-
+        <section className="flowers-container">
+            {renderFlowers()}
         </section>
     )
 }
