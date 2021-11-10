0747eff54bfdf6366a905b35c9d8f0343543d8fa
Diana Rybikov <diana.ryb214@gmail.com>
2021-11-10 16:36:22 -0700

    working

--- src/FlowersContainer.js
+++ src/FlowersContainer.js
@@ -2,11 +2,7 @@ import Flower from './Flower'
 
 export default function flowersContainer({flowers}) {
     const renderFlowers = () => {
-        return flowers.map(flower => {
-            return (
-                <Flower key={flower.id}/>
-            )
-        })
+        return flowers.map(flower => <Flower flower = {flower} key={flower.id}/> )
     }
     return(
         <section className="flowers-container">
