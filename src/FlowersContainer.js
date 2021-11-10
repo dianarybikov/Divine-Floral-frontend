16ff02e64f58109c64840ed106024c2a11551e8d
Diana Rybikov <diana.ryb214@gmail.com>
2021-11-10 16:34:30 -0700

    workingonflower

--- src/FlowersContainer.js
+++ src/FlowersContainer.js
@@ -1,8 +1,10 @@
+import Flower from './Flower'
+
 export default function flowersContainer({flowers}) {
     const renderFlowers = () => {
         return flowers.map(flower => {
             return (
-                
+                <Flower key={flower.id}/>
             )
         })
     }
