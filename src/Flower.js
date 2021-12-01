3ad0d6be823e96dd915d6c9ee9132b94e77fbecf
Diana Rybikov <diana.ryb214@gmail.com>
2021-12-01 16:39:29 -0700

    images-added

--- src/Flower.js
+++ src/Flower.js
@@ -3,6 +3,7 @@ return (
     <div className="flower-card">
         <h2>{flower.name}</h2>
         <p>cost: {flower.cost}</p> 
+        <img src={flower.image} alt={flower.image}/>
     </div>
     )
 }
